f4ddeef1dc94bafdbf3629c4ec2fec38af81125c
Lukas Krauch <lukas.krauch@yahoo.de>
2019-05-25 18:32:47 +0200

    Moved database initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:finewallet/history.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:finewallet/db_initilization.dart';
+import 'package:finewallet/Resources/db_initilization.dart';
 
 import 'add_page.dart';
 
