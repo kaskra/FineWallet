ed54dc639ccc32f13bdd0d75330e1619ad3c093d
Lukas Krauch <lukas.krauch@yahoo.de>
2019-05-27 15:50:18 +0200

    Extract day name switch to utils

--- lib/utils.dart
+++ lib/utils.dart
@@ -4,3 +4,33 @@ int dayInMillis(DateTime time) {
   DateTime newDay = DateTime.utc(time.year, time.month, time.day, 12);
   return newDay.millisecondsSinceEpoch;
 }
+
+String getDayName(int day) {
+  String dayName = "";
+  switch (day) {
+      case DateTime.monday:
+        dayName = "Monday";
+        break;
+      case DateTime.tuesday:
+        dayName = "Tuesday";
+        break;
+      case DateTime.wednesday:
+        dayName = "Wednesday";
+        break;
+      case DateTime.thursday:
+        dayName = "Thursday";
+        break;
+      case DateTime.friday:
+        dayName = "Friday";
+        break;
+      case DateTime.saturday:
+        dayName = "Saturday";
+        break;
+      case DateTime.sunday:
+        dayName = "Sunday";
+        break;
+      default:
+        dayName = "Today";
+    }
+  return dayName;
+}
