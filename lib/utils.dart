ac2546c1bfde604860dc720395dd194beff656ea
Lukas Krauch <lukas.krauch@yahoo.de>
2019-05-27 17:08:09 +0200

    Simplfiy method of day and month name

--- lib/utils.dart
+++ lib/utils.dart
@@ -11,40 +11,14 @@ int getLastDayOfMonth(DateTime date) {
 } 
 
 String getDayName(int day) {
-  String dayName = "";
-  switch (day) {
-      case DateTime.monday:
-        dayName = "Monday";
-        break;
-      case DateTime.tuesday:
-        dayName = "Tuesday";
-        break;
-      case DateTime.wednesday:
-        dayName = "Wednesday";
-        break;
-      case DateTime.thursday:
-        dayName = "Thursday";
-        break;
-      case DateTime.friday:
-        dayName = "Friday";
-        break;
-      case DateTime.saturday:
-        dayName = "Saturday";
-        break;
-      case DateTime.sunday:
-        dayName = "Sunday";
-        break;
-      default:
-        dayName = "Today";
-    }
-  return dayName;
+  List<String> days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  return days[(day-1) % 7];
 }
 
-String getMonthName(int month, [bool abbrev]){
+String getMonthName(int month, {bool abbrev=false}){
   List<String> months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  int index = month <= 0? 1 : month - 1;
-  month > 12? index = 12 : index = month;
-  return abbrev?months[index].substring(0,4) : months[index]; 
+  String monthName = months[(month - 1) % 12];
+  return abbrev?monthName.substring(0,3) : monthName; 
 }
 
 List<DateTime> getLastWeekAsDates() {
