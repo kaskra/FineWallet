80f6caffe6994b600f82daed71711e08f79138b9
Lukas Krauch <lukas.krauch@yahoo.de>
2019-05-28 15:06:06 +0200

    Make padding of general card an arg

--- lib/general_widgets.dart
+++ lib/general_widgets.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
   
-Widget generalCard(Widget child, [BoxDecoration decoration]){
+Widget generalCard(Widget child, [BoxDecoration decoration, double padding=10]){
   return Container(
     child: Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
       color: Colors.white,
       child: Container(
         decoration: decoration,
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(padding),
         child: child
       )
     )
