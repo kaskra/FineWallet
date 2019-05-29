e3953c3ca4a6fd85ad44919894a69191922d0150
Lukas Krauch <lukas.krauch@yahoo.de>
2019-05-29 23:17:26 +0200

    Auto format

--- lib/general_widgets.dart
+++ lib/general_widgets.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-  
-Widget generalCard(Widget child, [BoxDecoration decoration, double padding=10]){
+
+Widget generalCard(Widget child,
+    [BoxDecoration decoration, double padding = 10]) {
   return Container(
-    child: Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-      color: Colors.white,
-      child: Container(
-        decoration: decoration,
-        padding: EdgeInsets.all(padding),
-        child: child
-      )
-    )
-  );
+      child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+          color: Colors.white,
+          child: Container(
+              decoration: decoration,
+              padding: EdgeInsets.all(padding),
+              child: child)));
 }
