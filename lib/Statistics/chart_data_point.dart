2a28f735802f7bfa613f952d52f79b231763f8e8
Lukas Krauch <lukas.krauch@yahoo.de>
2019-06-10 21:15:29 +0200

    Extract data point type to own file

---
+++ lib/Statistics/chart_data_point.dart
@@ -0,0 +1,12 @@
+/*
+ * Developed by Lukas Krauch 10.6.2019.
+ * Copyright (c) 2019. All rights reserved.
+ *
+ */
+
+class DataPoint {
+  final int timeStamp;
+  final double expense;
+  final double income;
+  DataPoint(this.timeStamp, this.expense, this.income);
+}
