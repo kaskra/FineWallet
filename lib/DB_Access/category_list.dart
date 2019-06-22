60a86c27eba2e11e984e2566a611c5cf9d095bb1
Lukas Krauch <lukas.krauch@yahoo.de>
2019-06-22 17:56:36 +0200

    Add lists for other database tables too.

---
+++ lib/DB_Access/category_list.dart
@@ -0,0 +1,39 @@
+/*
+ * Developed by Lukas Krauch 22.6.2019.
+ * Copyright (c) 2019. All rights reserved.
+ *
+ */
+
+import 'dart:collection';
+
+import 'package:finewallet/Models/category_model.dart';
+
+CategoryList toCategoryList(List<CategoryModel> l) {
+  CategoryList list = new CategoryList();
+  list.addAll(l);
+  return list;
+}
+
+class CategoryList extends ListBase<CategoryModel> {
+  final List<CategoryModel> l = [];
+  CategoryList();
+
+  set length(int newLength) {
+    l.length = newLength;
+  }
+
+  int get length => l.length;
+  CategoryModel operator [](int index) => l[index];
+  operator []=(int index, CategoryModel value) {
+    l[index] = value;
+  }
+
+  CategoryList removeWhere(bool Function(CategoryModel element) test) {
+    l.removeWhere((CategoryModel txn) => test(txn));
+    return toCategoryList(l);
+  }
+
+  CategoryList where(bool Function(CategoryModel element) test) {
+    return toCategoryList(super.where(test).toList());
+  }
+}
---
+++ lib/DB_Access/subcategory_list.dart
@@ -0,0 +1,39 @@
+/*
+ * Developed by Lukas Krauch 22.6.2019.
+ * Copyright (c) 2019. All rights reserved.
+ *
+ */
+
+import 'dart:collection';
+
+import 'package:finewallet/Models/subcategory_model.dart';
+
+SubcategoryList toSubcategoryList(List<SubcategoryModel> l) {
+  SubcategoryList list = new SubcategoryList();
+  list.addAll(l);
+  return list;
+}
+
+class SubcategoryList extends ListBase<SubcategoryModel> {
+  final List<SubcategoryModel> l = [];
+  SubcategoryList();
+
+  set length(int newLength) {
+    l.length = newLength;
+  }
+
+  int get length => l.length;
+  SubcategoryModel operator [](int index) => l[index];
+  operator []=(int index, SubcategoryModel value) {
+    l[index] = value;
+  }
+
+  SubcategoryList removeWhere(bool Function(SubcategoryModel element) test) {
+    l.removeWhere((SubcategoryModel txn) => test(txn));
+    return toSubcategoryList(l);
+  }
+
+  SubcategoryList where(bool Function(SubcategoryModel element) test) {
+    return toSubcategoryList(super.where(test).toList());
+  }
+}
