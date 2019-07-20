8f67a583b096e88c36a8d63cf9656931d8907bea
Lukas Krauch <lukas.krauch@yahoo.de>
2019-07-21 00:17:32 +0200

    Add confirm dialog to general widgets

--- lib/general_widgets.dart
+++ lib/general_widgets.dart
@@ -33,6 +33,34 @@ Widget growAnimation(
   );
 }
 
+Future<bool> showConfirmDialog(
+    BuildContext context, String title, String content) {
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            FlatButton(
+              child: Text("Confirm"),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            )
+          ],
+        );
+      }).then((v) {
+    return v;
+  });
+}
+
 class DateSeparator extends StatelessWidget {
   final bool isToday;
   final int dateInMillis;
