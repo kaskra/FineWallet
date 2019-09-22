74f1cee0770fe70e9d58679c99886dbd34c663ae
Lukas Krauch <lukas.krauch@yahoo.de>
2019-09-22 18:06:33 +0200

    Add height and width to every ui helper

--- lib/src/widgets/ui_helper.dart
+++ lib/src/widgets/ui_helper.dart
@@ -1,51 +1,57 @@
 import 'package:flutter/material.dart';
 
-/// This widget will increase the child width to fit 
+/// This widget will increase the child width to fit
 /// into the specified ratio of the screen width.
 class ExpandToWidth extends StatelessWidget {
   const ExpandToWidth({
     Key key,
     @required this.child,
     this.ratio = 1,
+    this.height,
   }) : super(key: key);
 
   final Widget child;
   final double ratio;
+  final double height;
 
   @override
   Widget build(BuildContext context) => Container(
         margin: const EdgeInsets.symmetric(horizontal: 5),
         width: MediaQuery.of(context).size.width * ratio,
+        height: height,
         child: child,
       );
 }
 
-/// This widget will increase the child height to fit 
+/// This widget will increase the child height to fit
 /// into the specified ratio of the screen height.
 class ExpandToHeight extends StatelessWidget {
   const ExpandToHeight({
     Key key,
-    @required this.child, 
+    @required this.child,
     this.ratio = 1,
+    this.width,
   }) : super(key: key);
 
   final Widget child;
   final double ratio;
+  final double width;
 
   @override
   Widget build(BuildContext context) => Container(
         margin: const EdgeInsets.symmetric(vertical: 5),
         height: MediaQuery.of(context).size.height * ratio,
+        width: width,
         child: child,
       );
 }
 
-/// This widget will increase the child size to fit 
+/// This widget will increase the child size to fit
 /// into the specified ratio of the screen height and width.
 class ExpandToScreenRatio extends StatelessWidget {
   const ExpandToScreenRatio({
     Key key,
-    @required this.child, 
+    @required this.child,
     this.widthRatio = 0.5,
     this.heightRatio = 0.5,
   }) : super(key: key);
