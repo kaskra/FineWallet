5843e0b711db9f2305a7dded66ffaf863e520bd3
Lukas Krauch <lukas.krauch@yahoo.de>
2019-09-29 20:04:43 +0200

    Add simple text of database data to months

--- lib/src/statistics/stats.dart
+++ lib/src/statistics/stats.dart
@@ -6,30 +6,48 @@
  * Copyright 2019 - 2019 Sylu, Sylu
  */
 
+import 'package:FineWallet/core/models/month_model.dart';
+import 'package:FineWallet/core/resources/blocs/month_bloc.dart';
 import 'package:FineWallet/core/resources/transaction_list.dart';
 import 'package:FineWallet/src/widgets/decorated_card.dart';
 import 'package:FineWallet/src/widgets/ui_helper.dart';
 import 'package:FineWallet/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class StatisticsPage extends StatelessWidget {
   const StatisticsPage({Key key}) : super(key: key);
 
+  // TODO if needed, add page controller
   @override
   Widget build(BuildContext context) {
-    PageController _pageController = PageController(initialPage: 0);
-    return PageView.builder(
-      pageSnapping: true,
-      onPageChanged: (pageIndex) {
-        print(pageIndex);
-      },
-      scrollDirection: Axis.horizontal,
-      controller: _pageController,
-      itemBuilder: (context, index) {
-        return MonthCard(
-          context,
-          date: DateTime.now().add(Duration(days: 30 * index)),
-          transactions: TransactionList(),
+    return Consumer<MonthBloc>(
+      builder: (context, bloc, child) {
+        bloc.getMonths();
+        return StreamBuilder<List<MonthModel>>(
+          stream: bloc.allMonths,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return snapshot.hasData
+                ? PageView(
+                    pageSnapping: true,
+                    onPageChanged: (pageIndex) {
+                      print(pageIndex);
+                    },
+                    scrollDirection: Axis.horizontal,
+                    reverse: true,
+                    children: <Widget>[
+                      for (MonthModel m in snapshot.data)
+                        MonthCard(
+                          context: context,
+                          transactions: TransactionList(),
+                          model: m,
+                        )
+                    ],
+                  )
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  );
+          },
         );
       },
     );
@@ -38,12 +56,17 @@ class StatisticsPage extends StatelessWidget {
 
 class MonthCard extends StatelessWidget {
   final TransactionList transactions;
-  final DateTime date;
   final BuildContext context;
+  final MonthModel model;
+  final DateTime date;
 
-  const MonthCard(this.context,
-      {Key key, @required this.transactions, @required this.date})
-      : super(key: key);
+  MonthCard(
+      {Key key,
+      this.context,
+      @required this.transactions,
+      @required this.model})
+      : this.date = DateTime.fromMillisecondsSinceEpoch(model.firstDayOfMonth),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -66,12 +89,15 @@ class MonthCard extends StatelessWidget {
   }
 
   _buildContent() {
-    return Container(
-      child: _buildTitle(),
+    return Column(
+      children: <Widget>[
+        _buildTitle(),
+        _buildRest(),
+      ],
     );
   }
 
-  Center _buildTitle() {
+  Widget _buildTitle() {
     return Center(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -110,6 +136,19 @@ class MonthCard extends StatelessWidget {
               color: Theme.of(context).colorScheme.primary, fontSize: 22),
         ),
       ),
+    );
+  }
+
+  _buildRest() {
+    return Container(
+      child: Text("\n " +
+          date.toIso8601String() +
+          "\n Current max budget: " +
+          model.currentMaxBudget.toStringAsFixed(2) +
+          "\n Savings: " +
+          model.savings.toStringAsFixed(2) +
+          "\n Monthly expenses: " +
+          model.monthlyExpenses.toStringAsFixed(2)),
     );
   }
 }
