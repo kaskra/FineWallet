59076d9525dedc535eeb5abf6d2302e395c85726
Lukas Krauch <lukas.krauch@yahoo.de>
2019-11-01 16:42:03 +0100

    Update db initialization

--- lib/core/resources/blocs/month_bloc.dart
+++ lib/core/resources/blocs/month_bloc.dart
@@ -114,8 +114,8 @@ class MonthBloc {
       MonthProvider.db.updateMonth(m);
     }
 
-    getCurrentMonth();
-    getMonths();
-    getSavings();
+    await getCurrentMonth();
+    await getMonths();
+    await getSavings();
   }
 }
