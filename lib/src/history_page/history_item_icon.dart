12c4f8f817f0cc82634e57937d69a6bf144dbb92
Lukas Krauch <lukas.krauch@yahoo.de>
2019-11-08 23:18:02 +0100

    Make clip radius a class argument

--- lib/src/history_page/history_item_icon.dart
+++ lib/src/history_page/history_item_icon.dart
@@ -11,13 +11,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class IconWrapper extends StatelessWidget {
-  const IconWrapper(
-      {Key key, @required this.child, this.alignment = Alignment.topLeft, this.padding = const EdgeInsets.only(right: 10)})
-      : super(key: key);
+  const IconWrapper({
+    Key key,
+    @required this.child,
+    this.alignment = Alignment.topLeft,
+    this.padding = const EdgeInsets.only(right: 10),
+    this.clipRadius = 25,
+  }) : super(key: key);
 
   final Widget child;
   final Alignment alignment;
   final EdgeInsetsGeometry padding;
+  final double clipRadius;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +31,7 @@ class IconWrapper extends StatelessWidget {
       child: Align(
         alignment: alignment,
         child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(25)),
+          borderRadius: BorderRadius.all(Radius.circular(clipRadius)),
           child: child,
         ),
       ),
