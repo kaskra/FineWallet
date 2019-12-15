e7f5fcd023a44e1222667e31fa95a15ab0435431
Lukas Krauch <lukas.krauch@yahoo.de>
2019-12-15 22:51:55 +0100

    Toggle between color scheme by pressing switch
    Issue: Switch is not updating yet
    Issue: Setting is not getting persistently saved

--- lib/src/settings_page/appearance_section.dart
+++ lib/src/settings_page/appearance_section.dart
@@ -1,5 +1,7 @@
+import 'package:FineWallet/data/providers/theme_notifier.dart';
 import 'package:FineWallet/src/widgets/section.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /// This class creates a [Section] which shows the appearance
 /// settings, like Dark Mode.
@@ -8,18 +10,19 @@ class AppearanceSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Section(
       children: <SectionItem>[
-        _buildDarkModeSwitch(),
+        _buildDarkModeSwitch(context),
       ],
     );
   }
 
-  static Widget _buildDarkModeSwitch() {
+  static Widget _buildDarkModeSwitch(BuildContext context) {
     return SectionItem(
       title: "Dark Mode",
       trailing: Switch(
         value: false,
         onChanged: (val) {
           print("Toggle Dark Mode");
+          Provider.of<ThemeNotifier>(context).switchTheme();
         },
       ),
     );
