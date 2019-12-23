23d601067997d4f2fe9ff38133463c561bc06cee
Lukas Krauch <lukas.krauch@yahoo.de>
2019-12-23 13:24:31 +0100

    Move widgets to own files

--- lib/src/add_page/editable_numeric_input.dart
+++ lib/src/add_page/editable_numeric_input.dart
@@ -4,46 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-/// This class creates a [Divider] that is used to divide the row
-/// parent from its children.
-class RowChildDivider extends StatelessWidget {
-  const RowChildDivider({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Divider(
-      color: Theme.of(context).colorScheme.onBackground,
-      indent: 20,
-      endIndent: 15,
-      thickness: 0.2,
-      height: 1,
-    );
-  }
-}
-
-/// This class is used to create a specific [Text] widget right above the row.
-class RowTitle extends StatelessWidget {
-  const RowTitle({
-    Key key,
-    @required this.title,
-  }) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 48),
-      child: Text(
-        title,
-        style: TextStyle(fontSize: 13),
-      ),
-    );
-  }
-}
-
 class EditableNumericInputText extends StatefulWidget {
   final double defaultValue;
 
