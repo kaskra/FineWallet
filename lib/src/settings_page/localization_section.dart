3ab4131cbd70ad0463626a7afd59203ee78fed79
Lukas Krauch <lukas.krauch@yahoo.de>
2019-12-28 13:24:28 +0100

    Add selection page for currency

--- lib/src/settings_page/localization_section.dart
+++ lib/src/settings_page/localization_section.dart
@@ -1,6 +1,6 @@
 import 'package:FineWallet/data/providers/localization_notifier.dart';
 import 'package:FineWallet/data/user_settings.dart';
-import 'package:FineWallet/src/settings_page/localization_pages/language_selection_page.dart';
+import 'package:FineWallet/src/settings_page/localization_pages/selection_page.dart';
 import 'package:FineWallet/src/widgets/section.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -32,23 +32,33 @@ class _LocalizationSectionState extends State<LocalizationSection> {
     return Section(
       title: "Localization",
       children: <SectionItem>[
-        _buildLanguage2(),
+        _buildLanguage(),
         _buildCurrency(),
       ],
     );
   }
 
-  Widget _buildLanguage2() {
+  Widget _buildLanguage() {
+    // TODO remove, just placeholders
+    Map<int, String> items = Map();
+    items.putIfAbsent(1, () => "ENG");
+    items.putIfAbsent(2, () => "GER");
+
     return SectionItem(
       title: "Language (UNUSED)",
       // TODO write language name in front of arrow (get database here and send snapshot to other page)
       trailing: Material(
         color: Colors.transparent,
         child: InkWell(
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => LanguageSelectionPage(
-                    selectedIndex: UserSettings.getLanguage())));
+          onTap: () async {
+            var res = await Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => SelectionPage(
+                      selectedIndex: UserSettings.getLanguage(),
+                      data: items,
+                    )));
+            if (res != null) {
+              UserSettings.setLanguage(res);
+            }
           },
           child: Icon(
             Icons.keyboard_arrow_right,
@@ -59,28 +69,33 @@ class _LocalizationSectionState extends State<LocalizationSection> {
   }
 
   Widget _buildCurrency() {
+    // TODO just placeholders
+    // TODO String should be currency abbrev., not symbol
+    Map<int, String> items = Map();
+    items.putIfAbsent(1, () => "\$");
+    items.putIfAbsent(2, () => "€");
+
     return SectionItem(
       title: "Currency Symbol",
-      trailing: DropdownButtonHideUnderline(
-        child: DropdownButton(
-          value: _selectedCurrency,
-          isDense: true,
-          onChanged: (val) {
-            Provider.of<LocalizationNotifier>(context).setCurrencySymbol(val);
-            setState(() {
-              _selectedCurrency = val;
-            });
+      // TODO write currency symbol in front of arrow (get database here and send snapshot to other page)
+      trailing: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () async {
+            var res = await Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => SelectionPage(
+                      selectedIndex: UserSettings.getCurrency(),
+                      data: items,
+                    )));
+            if (res != null) {
+              UserSettings.setCurrency(res);
+              // TODO get symbol from database and provide it
+              Provider.of<LocalizationNotifier>(context).setCurrencySymbol(res);
+            }
           },
-          items: [
-            DropdownMenuItem(
-              child: Text("\$"),
-              value: 1,
-            ),
-            DropdownMenuItem(
-              child: Text("€"),
-              value: 2,
-            ),
-          ],
+          child: Icon(
+            Icons.keyboard_arrow_right,
+          ),
         ),
       ),
     );
