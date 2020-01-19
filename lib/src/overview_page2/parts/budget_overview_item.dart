585ffdf0d14754bdb1863dac234e4aa8b90ac95f
Lukas Krauch <lukas.krauch@yahoo.de>
2020-01-19 18:55:47 +0100

    Add navigation button to budget overview

--- lib/src/overview_page2/parts/budget_overview_item.dart
+++ lib/src/overview_page2/parts/budget_overview_item.dart
@@ -1,8 +1,10 @@
+import 'package:FineWallet/data/providers/navigation_notifier.dart';
 import 'package:FineWallet/src/widgets/views/daily_budget.dart';
 import 'package:FineWallet/src/widgets/views/monthly_budget.dart';
 import 'package:FineWallet/src/widgets/views/savings.dart';
 import 'package:FineWallet/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class BudgetOverviewItem extends StatelessWidget {
   /// Builds the child widget into a container with a set padding.
@@ -23,43 +25,13 @@ class BudgetOverviewItem extends StatelessWidget {
           Text(
             footerText,
             style: TextStyle(
-              fontSize: 12,
+              fontSize: 13,
               fontWeight: FontWeight.w500,
               color: Theme.of(context).colorScheme.secondary,
             ),
           ),
         ],
       ),
-    );
-  }
-
-  Widget _buildMonth(BuildContext context) {
-    return _buildRowItem(
-      context,
-      child: MonthlyBudgetView(
-        textStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-      ),
-      footerText: getMonthName(DateTime.now().month).toString(),
-    );
-  }
-
-  Widget _buildToday(BuildContext context) {
-    return _buildRowItem(
-      context,
-      child: DailyBudgetView(
-        textStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-      ),
-      footerText: "Today",
-    );
-  }
-
-  Widget _buildSavings(BuildContext context) {
-    return _buildRowItem(
-      context,
-      child: SavingsView(
-        textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-      ),
-      footerText: "Saved amount",
     );
   }
 
@@ -72,21 +44,58 @@ class BudgetOverviewItem extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              _buildToday(context),
-              _buildMonth(context),
+              _buildRowItem(
+                context,
+                child: DailyBudgetView(
+                  textStyle:
+                      TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                ),
+                footerText: "Today",
+              ),
+              _buildRowItem(
+                context,
+                child: MonthlyBudgetView(
+                  textStyle:
+                      TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                ),
+                footerText: getMonthName(DateTime.now().month).toString(),
+              ),
             ],
           ),
           Container(
-            margin: const EdgeInsets.only(top: 20),
+            margin: const EdgeInsets.only(top: 20, bottom: 16),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                _buildSavings(context),
+                _buildRowItem(
+                  context,
+                  child: SavingsView(
+                    textStyle:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  footerText: "Saved amount",
+                ),
               ],
             ),
-          )
+          ),
+          _buildNavigationButton(context)
         ],
       ),
+    );
+  }
+
+  MaterialButton _buildNavigationButton(BuildContext context) {
+    return MaterialButton(
+      shape: RoundedRectangleBorder(),
+      onPressed: () {
+        Provider.of<NavigationNotifier>(context).setPage(0);
+      },
+      child: Text(
+        "Change Available Budget",
+        style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+      ),
+      minWidth: MediaQuery.of(context).size.width * 0.9,
+      color: Theme.of(context).colorScheme.primary,
     );
   }
 }
