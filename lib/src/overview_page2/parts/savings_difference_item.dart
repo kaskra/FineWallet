d19fe69c94f2565a89249c24a93eec4fb0f4747f
Lukas Krauch <lukas.krauch@yahoo.de>
2020-02-21 17:03:12 +0100

    Add image to savings difference overview

--- lib/src/overview_page2/parts/savings_difference_item.dart
+++ lib/src/overview_page2/parts/savings_difference_item.dart
@@ -1,30 +1,47 @@
 import 'package:FineWallet/src/overview_page2/parts/row_item.dart';
 import 'package:FineWallet/src/widgets/decorated_card.dart';
+import 'package:FineWallet/src/widgets/structure/structure_space.dart';
 import 'package:FineWallet/src/widgets/views/savings.dart';
 import 'package:FineWallet/src/widgets/views/savings_difference.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SavingsDifferenceItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DecoratedCard(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Column(
         children: <Widget>[
-          RowItem(
-            child: SavingsView(
-              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: SvgPicture.asset(
+              "assets/savings.svg",
+              height: 100,
+              semanticsLabel: "Savings",
             ),
-            padding: const EdgeInsets.symmetric(vertical: 4),
-            footerText: "Saved amount",
           ),
-          RowItem(
-            child: SavingsDifferenceView(
-              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-            padding: const EdgeInsets.symmetric(vertical: 4),
-            footerText: "Currently",
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              RowItem(
+                child: SavingsView(
+                  textStyle:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                footerText: "Saved amount",
+              ),
+              RowItem(
+                child: SavingsDifferenceView(
+                  textStyle:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                footerText: "Currently",
+              ),
+            ],
           ),
+          StructureSpace()
         ],
       ),
     );
