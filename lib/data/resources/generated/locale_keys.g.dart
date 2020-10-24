d6337403d88e5f30011f432322a739dcb6e5ec96
Lukas Krauch <lukas.krauch@yahoo.de>
2020-10-24 17:07:50 +0200

    Update gitignore to commit generated files

---
+++ lib/data/resources/generated/locale_keys.g.dart
@@ -0,0 +1,30 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const nav_home = 'nav.home';
+  static const nav_me = 'nav.me';
+  static const nav_history = 'nav.history';
+  static const nav_statistics = 'nav.statistics';
+  static const nav = 'nav';
+  static const welcome_pages_welcome_title = 'welcome_pages.welcome.title';
+  static const welcome_pages_welcome_next_steps = 'welcome_pages.welcome.next_steps';
+  static const welcome_pages_welcome_data_1 = 'welcome_pages.welcome.data_1';
+  static const welcome_pages_welcome_data_bold = 'welcome_pages.welcome.data_bold';
+  static const welcome_pages_welcome_data_2 = 'welcome_pages.welcome.data_2';
+  static const welcome_pages_welcome = 'welcome_pages.welcome';
+  static const welcome_pages_currency_title = 'welcome_pages.currency.title';
+  static const welcome_pages_currency_text = 'welcome_pages.currency.text';
+  static const welcome_pages_currency_warning = 'welcome_pages.currency.warning';
+  static const welcome_pages_currency = 'welcome_pages.currency';
+  static const welcome_pages_dark_mode_title = 'welcome_pages.dark_mode.title';
+  static const welcome_pages_dark_mode_text = 'welcome_pages.dark_mode.text';
+  static const welcome_pages_dark_mode = 'welcome_pages.dark_mode';
+  static const welcome_pages_finish_title = 'welcome_pages.finish.title';
+  static const welcome_pages_finish_text = 'welcome_pages.finish.text';
+  static const welcome_pages_finish = 'welcome_pages.finish';
+  static const welcome_pages = 'welcome_pages';
+  static const continueText = 'continueText';
+  static const backText = 'backText';
+  static const doneText = 'doneText';
+
+}
