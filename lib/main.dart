97bf9b320cc8b1c6dcdcfbdd5466c5827c2fc1d0
Lukas Krauch <lukas.krauch@yahoo.de>
2021-03-23 16:34:17 +0100

    Extend loading page duration

--- lib/main.dart
+++ lib/main.dart
@@ -196,7 +196,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future _initStates() async {
     await _loadBudget();
     await _loadLocalizationAndCurrency();
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 5));
   }
 
   PreferredSizeWidget _chooseAppBar(BuildContext context) {
